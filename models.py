d03692ac7ec0b179b64d8ed61a164f141a91cb17
Lillian Noordhoek <lilliann@wustl.edu>
2023-11-21 10:26:20 -0600

    Adding user class

--- models.py
+++ models.py
@@ -3,6 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """
+    ! This is not meant to hold any sensitive user info, it's just a way for users to store their win/loss history, if they want to. 
+    """
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.Text,nullable=False)
+    passwordUnsecure = db.Column(db.Text,nullable=False) 
+    winCount = db.Column(db.Integer)
+    lossCount = db.Column(db.Integer)
+    
+    @property
+    def calculateWinLossRatio(self):
+        winLossRatio = float(self.winCount / self.lossCount)
+        return winLossRatio
 
 def connect_db(app):
     db.app = app
