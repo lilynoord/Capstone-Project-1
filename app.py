ccffa6a497d8a41494995caa5175fcdc08cf68cc
Lillian <lilynoordhoek@gmail.com>
2024-04-10 11:37:12 -0500

    adjusting stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from __future__ import print_function
+
 from tools import *
 from flask import Flask, redirect, render_template, flash, request, make_response
 from sqlalchemy import select
@@ -55,7 +55,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "it's a secret")
-toolbar = DebugToolbarExtension(app)
+
 
 connect_db(app)
 
